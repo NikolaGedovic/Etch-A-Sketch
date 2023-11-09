3f0913518ea620bead1af118b771b4a210d08bd6
Nikola <gedovicc96@gmail.com>
2023-11-09 19:21:00 +0100

    creating a grid 16 x 16 in script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.querySelector('#container');
+
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      container.appendChild(cell);
+
+      cell.addEventListener('mouseover', function () {
+        this.style.backgroundColor = '#000000';
+      });
+    }
+  }
+});
