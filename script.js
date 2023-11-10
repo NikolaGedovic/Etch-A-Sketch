a6d47e01a148a5e5756e3c9320fa8be132b52623
Nikola <gedovicc96@gmail.com>
2023-11-10 18:11:47 +0100

    finish fixing bugs with eraser button and removing clear button

--- script.js
+++ script.js
@@ -1,15 +1,113 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const container = document.querySelector('#container');
+  const container = document.querySelector('#grid-container');
+  const colorInput = document.querySelector('#color-input');
+  const sizeValue = document.querySelector('#size-value');
+  const rangeInput = document.querySelector('#range-input');
+  const eraserButton = document.querySelector('#eraser-btn');
+  const rainbowButton = document.querySelector('#rainbow-btn');
+  const originalColorButton = document.querySelector('#color-btn');
 
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      container.appendChild(cell);
+  let originalColor = '#252B48';
+  let rainbowModeActive = false;
+  let eraserModeActive = false;
+  let previousColor = originalColor;
 
-      cell.addEventListener('mouseover', function () {
-        this.style.backgroundColor = '#000000';
-      });
+  // Initial grid setup
+  createGrid(16);
+
+  // Add event listener for the range input
+  rangeInput.addEventListener('input', function () {
+    const newSize = parseInt(rangeInput.value);
+    clearGrid();
+    createGrid(newSize);
+    updateSizeValue(newSize);
+    updateButtonStates();
+  });
+
+  // Add event listener for the eraser button
+  eraserButton.addEventListener('click', function () {
+    colorInput.value = '#ffffff';
+    eraserModeActive = true;
+    rainbowModeActive = false;
+    updateButtonStates();
+  });
+
+  // Add event listener for the rainbow button
+  rainbowButton.addEventListener('click', function () {
+    rainbowModeActive = true;
+    eraserModeActive = false;
+    updateButtonStates();
+  });
+
+  // Add event listener for the original color button
+  originalColorButton.addEventListener('click', function () {
+    colorInput.value = previousColor;
+    eraserModeActive = false;
+    rainbowModeActive = false;
+    updateButtonStates();
+  });
+
+  // Add event listener for the color input
+  colorInput.addEventListener('input', function () {
+    previousColor = colorInput.value;
+    updateButtonStates();
+  });
+
+  function createGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+
+        square.addEventListener('mouseover', function () {
+          if (eraserModeActive) {
+            this.style.backgroundColor = '#ffffff';
+          } else if (rainbowModeActive) {
+            this.style.backgroundColor = getRandomColor();
+          } else {
+            this.style.backgroundColor = colorInput.value;
+          }
+        });
+      }
     }
+  }
+
+  function clearGrid() {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+
+  function updateSizeValue(size) {
+    sizeValue.textContent = `${size} x ${size}`;
+  }
+
+  function updateButtonStates() {
+    if (eraserModeActive) {
+      eraserButton.classList.add('active');
+      rainbowButton.classList.remove('active');
+      originalColorButton.classList.remove('active');
+    } else if (rainbowModeActive) {
+      eraserButton.classList.remove('active');
+      rainbowButton.classList.add('active');
+      originalColorButton.classList.remove('active');
+    } else {
+      eraserButton.classList.remove('active');
+      rainbowButton.classList.remove('active');
+      originalColorButton.classList.remove('active');
+    }
+  }
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 });
